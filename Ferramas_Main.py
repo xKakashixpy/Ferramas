7b28790a275d401bc05f232be7af51e2c1ae3385
Javier Solar <114122506+xKakashixpy@users.noreply.github.com>
2024-05-18 23:07:47 -0400

    Convertidor de moneda Funcioando 100

--- Ferramas_Main.py
+++ Ferramas_Main.py
@@ -1,29 +1,36 @@
 import bcapi
 from flask import Flask, render_template, request, jsonify
 
-
 app = Flask(__name__)
 
-
-#Ruta para usar el convertidor
+# Ruta para mostrar el conversor de moneda
 @app.route('/tipocambio')
 def conversor():
     tasa_usd_clp = bcapi.obtener_tasa_de_cambio()
     return render_template('tipocambio.html', tasa_usd_clp=tasa_usd_clp)
 
-# convertidor de moneda 
+# Ruta para manejar la conversión de moneda
 @app.route('/convertir', methods=['POST'])
-
 def convertir():
     try:
         datos = request.get_json()
         monto = float(datos['monto'])
-        moneda = datos['moneda']
+        moneda1 = datos['moneda1']
+        moneda2 = datos['moneda2']
         tasa_usd_clp = bcapi.obtener_tasa_de_cambio()
+        
         if tasa_usd_clp:
-            resultado = bcapi.convertir_moneda(monto, moneda, tasa_usd_clp)
+            if moneda1 == "usd" and moneda2 == "clp":
+                resultado = bcapi.convertir_moneda(monto, 'usd', tasa_usd_clp)
+            elif moneda1 == "clp" and moneda2 == "usd":
+                resultado = bcapi.convertir_moneda(monto, 'clp', tasa_usd_clp)
+            else:
+                resultado = "Conversión no válida"
             return jsonify({'resultado': resultado})
         else:
             return jsonify({'error': 'No se pudo obtener la tasa de cambio'}), 500
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- bcapi.py
+++ bcapi.py
@@ -1,26 +1,21 @@
 import bcchapi
 
-
 def obtener_tasa_de_cambio():
     try:
         siete = bcchapi.Siete("ba.vidals@duocuc.cl", "Wani-gushi007")
         tipo_de_cambio = siete.cuadro(
             series=["F073.TCO.PRE.Z.D"],
             nombres=["tipo_de_cambio_dolar"],
-            desde="2023-01-01",  
+            desde="2024-01-01",  
             hasta="2024-12-01",
             frecuencia="D",
             observado={"tipo_de_cambio_dolar": "last"}
         )
-
-        # Extrae el valor de la tasa de cambio
         tasa = tipo_de_cambio['tipo_de_cambio_dolar'].iloc[-1]
-        print(f"Valor actual del dólar: {tasa} CLP")  # Muestra el valor actual del dólar
         return tasa
     except Exception as e:
         print("Error al obtener la tasa de cambio:", e)
         return None
-    
 
 def convertir_moneda(monto, moneda, tasa_usd_clp):
     if moneda.lower() == 'usd':
@@ -30,34 +25,3 @@ def convertir_moneda(monto, moneda, tasa_usd_clp):
     else:
         resultado = 'Moneda no válida'
     return resultado
-
-def main():
-    tasa_usd_clp = obtener_tasa_de_cambio()  # Obtiene la tasa de cambio una sola vez al inicio
-    if tasa_usd_clp is None:
-        return  # Finaliza el programa si no se puede obtener la tasa de cambio
-
-    while True:
-        try:
-            monto = float(input("Ingrese el monto a convertir: "))
-            moneda = input("Ingrese la moneda de origen (USD o CLP): ")
-
-            if moneda.lower() not in ['usd', 'clp']:
-                print("Moneda no válida. Por favor, ingrese 'USD' o 'CLP'.")
-                continue
-
-            resultado = convertir_moneda(monto, moneda, tasa_usd_clp)
-            if isinstance(resultado, str):
-                print(resultado)
-
-            else:
-                # Aquí formateamos el resultado como moneda con separadores de miles
-                resultado_formateado = "{:,.0f}".format(resultado).replace(",", ".")
-                print(f"El monto convertido es: ${resultado_formateado} ")
-            continuar = input("¿Desea realizar otra conversión? (s/n): ")
-            if continuar.lower() != 's':
-                break
-        except ValueError:
-            print("Por favor, ingrese un número válido.")
-
-if __name__ == "__main__":
-    main()
